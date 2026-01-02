d64ac6c26282f7ce826655e5a52c1cd0d0bfb8f1
Ansh Kataria <ansh.kataria@student.uq.edu.au>
2026-01-03 07:16:55 +1000

    initial commit: project structure

---
+++ src/main/java/com/githubUserActivity/githubUserActivity/GithubUserActivityApplication.java
@@ -0,0 +1,15 @@
+package com.githubUserActivity.githubUserActivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubUserActivityApplication {
+
+	public static void main(String[] args)
+
+    {
+		SpringApplication.run(GithubUserActivityApplication.class, args);
+	}
+
+}
